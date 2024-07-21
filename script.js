7fcd4e5a7d54628581930b1bfe0b59eca6175a99
rt22083j <rt22083j@st.omu.ac.jp>
2024-07-21 20:23:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-let slideIndex = 0;
+let slideIndex = 1;
 showSlides(slideIndex);
-
-function changeSlide(n) {
-    showSlides(slideIndex += n);
-}
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("slide");
-    if (n >= slides.length) {
-        slideIndex = 0;
-    }
-    if (n < 0) {
-        slideIndex = slides.length - 1;
-    }
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex].style.display = "block";
+    slides[slideIndex-1].style.display = "block";
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
 }
